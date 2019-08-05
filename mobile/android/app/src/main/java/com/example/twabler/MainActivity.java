ca171d64344a4f6655a6b684696609daa2e7e90f
Irina Kolobova <kolobovairinav@gmail.com>
2019-08-05 16:14:35 -0700

    Add basic app implementation according to mockups

--- mobile/android/app/src/main/java/com/example/twabler/MainActivity.java
+++ mobile/android/app/src/main/java/com/example/twabler/MainActivity.java
@@ -1,13 +1,94 @@
 package com.example.twabler;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView_tweetToTag, textView_tagTweet;
+    private ListView listView_tags;
+    private Button button_skip, button_submit;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView_tweetToTag = findViewById(R.id.textView_tweetToTag);
+        textView_tagTweet = findViewById(R.id.textView_tagTweet);
+        listView_tags = findViewById(R.id.listView_tags);
+        button_skip = findViewById(R.id.button_skip);
+        button_submit = findViewById(R.id.button_submit);
+        drawer = findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        //toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorTrueWhite));
+
+       /* ArrayList<String> tags = new ArrayList();
+        tags.add("Lorem");
+        tags.add("ipsum");
+        tags.add("dolor");
+        tags.add("sit");
+        tags.add("amet");
+        tags.add("consectetur");
+        tags.add("adipiscing");
+        tags.add("elit");
+        tags.add("sed");*/
     }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void skip(View view) {
+    }
+
+    public void submit(View view) {
+    }
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_add_contact:
+                Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_instructions:
+                Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_faq:
+                Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    } */
 }
