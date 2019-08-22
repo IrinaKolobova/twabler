1cdf338d2b4a1c36ad0b96d9c20ba0c8a511bdbd
Irina Kolobova <kolobovairinav@gmail.com>
2019-08-21 18:52:37 -0700

    Add test version of dynamic TextViews.

--- mobile/android/app/src/main/java/com/example/twabler/MainActivity.java
+++ mobile/android/app/src/main/java/com/example/twabler/MainActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
@@ -17,21 +20,24 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView_tweetToTag, textView_tagTweet;
-    private ListView listView_tags;
+    private TextView textView_tweetToTag;
     private Button button_skip, button_submit;
     private DrawerLayout drawer;
+
+    int numberOfWords;
+    ArrayList<String> twitText = new ArrayList<>();
+    LinearLayout tagLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView_tweetToTag = findViewById(R.id.textView_tweetToTag);
-        textView_tagTweet = findViewById(R.id.textView_tagTweet);
-        listView_tags = findViewById(R.id.listView_tags);
         button_skip = findViewById(R.id.button_skip);
         button_submit = findViewById(R.id.button_submit);
         drawer = findViewById(R.id.drawer_layout);
+
+        tagLayout = findViewById(R.id.tagLayout);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,17 +47,40 @@ public class MainActivity extends AppCompatActivity {
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         //toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorTrueWhite));
+        addWords();
+        addTextViews();
 
-       /* ArrayList<String> tags = new ArrayList();
-        tags.add("Lorem");
-        tags.add("ipsum");
-        tags.add("dolor");
-        tags.add("sit");
-        tags.add("amet");
-        tags.add("consectetur");
-        tags.add("adipiscing");
-        tags.add("elit");
-        tags.add("sed");*/
+
+    }
+
+    public void addWords(){
+        //hardcoded! in the future get words from json
+        twitText.add("Lorem");
+        twitText.add("ipsum");
+        twitText.add("dolor");
+        twitText.add("sit");
+        twitText.add("amet");
+        twitText.add("consectetur");
+        twitText.add("adipiscing");
+        twitText.add("elit");
+        twitText.add("sed");
+
+        numberOfWords = twitText.size();
+    }
+
+    //test version (just to make dynamic text views work)
+    public void addTextViews(){
+
+        final TextView[] tagTextViews = new TextView[numberOfWords];
+        for (int i = 0; i < numberOfWords; i++) {
+            TextView wordTextView = new TextView(this);
+            wordTextView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 45));
+            wordTextView.setGravity(Gravity.CENTER_VERTICAL);
+            wordTextView.setPadding(0, 0, 20, 0);
+            wordTextView.setText(twitText.get(i));
+            tagLayout.addView(wordTextView);
+            tagTextViews[i] = wordTextView;
+        }
     }
 
     @Override
